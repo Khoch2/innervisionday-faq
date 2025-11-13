6fba437d04f6828aae0aedc980ff7cdd0c888e26
Khoch2 <thomas@daniel-kurzeja.de>
2025-11-13 12:17:28 +0100

    Fix secure wss socket

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,9 +1,10 @@
 import { io } from "socket.io-client";
 
+// sichere URL automatisch ableiten
 const URL =
   import.meta.env.VITE_SOCKET_URL ||
-  window.location.origin.replace(/^http/, "ws");
+  window.location.origin.replace(/^http/, "wss");
 
 export const socket = io(URL, {
-  transports: ["websocket"],
+  transports: ["websocket"]
 });
